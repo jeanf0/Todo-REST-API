781f5c13779301739fd42d9565e9706b41edf579
Jean Fraga <jeanfraga11@gmail.com>
2023-10-25 19:30:37 -0300

    Created TodoVO and a Mapper

---
+++ src/main/java/com/jeanfraga/mapper/Mapper.java
@@ -0,0 +1,23 @@
+package com.jeanfraga.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+public class Mapper {
+	
+	private static ModelMapper mapper = new ModelMapper();
+	
+	public static <O, D> D parseObject(O origin, Class<D> destination) {
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+		List<D> destinationList = new ArrayList<>();
+		for (O o : origin) {
+			destinationList.add(mapper.map(o, destination));
+		}
+		return destinationList;
+	}
+}
---
+++ src/main/java/com/jeanfraga/vo/TodoVO.java
@@ -0,0 +1,80 @@
+package com.jeanfraga.vo;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class TodoVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private boolean realizado;
+	private Integer prioridade;
+	
+	public TodoVO() {};
+	
+	public TodoVO(Long id, String nome, String descricao, boolean realizado, Integer prioridade) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.realizado = realizado;
+		this.prioridade = prioridade;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isRealizado() {
+		return realizado;
+	}
+	public void setRealizado(boolean realizado) {
+		this.realizado = realizado;
+	}
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, nome, prioridade, realizado);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TodoVO other = (TodoVO) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(prioridade, other.prioridade)
+				&& realizado == other.realizado;
+	}
+}
