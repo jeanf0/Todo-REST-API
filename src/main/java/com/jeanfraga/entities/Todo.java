12960852dd4354af6212a4c95341151f6e408616
Jean Fraga <jeanfraga11@gmail.com>
2023-10-25 19:03:43 -0300

    Created Todo entity

---
+++ src/main/java/com/jeanfraga/entities/Todo.java
@@ -0,0 +1,82 @@
+package com.jeanfraga.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private boolean realizado;
+	private Integer prioridade;
+	
+	public Todo() {};
+	
+	public Todo(Long id, String nome, String descricao, boolean realizado, Integer prioridade) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.realizado = realizado;
+		this.prioridade = prioridade;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isRealizado() {
+		return realizado;
+	}
+	public void setRealizado(boolean realizado) {
+		this.realizado = realizado;
+	}
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, nome, prioridade, realizado);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(prioridade, other.prioridade)
+				&& realizado == other.realizado;
+	}
+}
